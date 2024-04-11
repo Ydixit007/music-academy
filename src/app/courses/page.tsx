8f60f47b5c6e6b3c7a376aedc6a80bdf33cb4bc0
Yash Dixit <ydixit007@gmail.com>
2024-04-11 22:44:31 +0530

    course page completed

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import CourseLanding from "@/components/CourseLanding";
+import FeaturedCard from "@/components/FeaturedCard";
+import courseData from "@/data/music_courses.json";
+
+interface Course {
+  id: number;
+  title: string;
+  slug: string;
+  description: string;
+  price: number;
+  instructor: string;
+  isFeatured: boolean;
+  image: string;
+}
 
 const Page = () => {
   return (
-    <div className='min-h-screen w-full flex justify-center items-center md:px-0 px-8 py-10'>Courses</div>
-  )
-}
+    <div className="min-h-screen w-full flex flex-col">
+      <CourseLanding />
+      <div className="courses md:px-20 px-4 py-10 min-h-[40rem]">
+        <h1 className="text-2xl text-gray-300 font-semibold tracking-wide uppercase mb-8">
+          Discover courses
+        </h1>
+        <div className="courses-grid grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-full gap-12">
+          {courseData.courses.map((course: Course) => (
+            <FeaturedCard course={course} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Page
+export default Page;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -51,7 +51,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [addVariablesForColors, backgroundSvg],
+  plugins: [addVariablesForColors, backgroundSvg, bgDotsThick],
 };
 export default config;
 
@@ -82,6 +82,19 @@ function backgroundSvg({ matchUtilities, theme }: any) {
       "bg-dot": (value: any) => ({
         backgroundImage: `url("${svgToDataUri(
           `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
+        )}")`,
+      }),
+    },
+    { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
+  );
+}
+
+function bgDotsThick ({ matchUtilities, theme }: any) {
+  matchUtilities(
+    {
+      "bg-dot-thick": (value: any) => ({
+        backgroundImage: `url("${svgToDataUri(
+          `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="2.5"></circle></svg>`
         )}")`,
       }),
     },
