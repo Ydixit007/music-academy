44775d61e88a71b89f1eeb2125d1c8fc3ca6377c
Yash Dixit <ydixit007@gmail.com>
2024-04-11 22:16:48 +0530

    contact form updated

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,11 +1,11 @@
+import ContactForm from "@/components/ContactForm";
 import { BackgroundBeams } from "@/components/ui/background-beams";
 
 export default function page() {
     return (
-        <div className="min-h-screen relative flex justify-center items-center">
-            <h1>
-                Contact page
-            </h1>
+        <div className="min-h-screen relative flex flex-col items-center justify-center">
+            <h2 className="mt-2 text-3xl leading-8 font-extrabold tracking-tight sm:text-4xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">Get in touch</h2>
+            <ContactForm />
             <BackgroundBeams />
         </div>
     );
